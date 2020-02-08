c2834c823c4b8ca9fe3204b9ca78c43b76d1e287
Comma Device <device@comma.ai>
2020-02-08 21:54:18 +0000

    Suppressed automatic updates

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -29,31 +29,31 @@ function launch {
   # 2. The FINALIZED consistent file has to exist, indicating there's an update
   #    that completed successfully and synced to disk.
 
-  if [ -f "${BASEDIR}/.overlay_init" ]; then
-    find ${BASEDIR}/.git -newer ${BASEDIR}/.overlay_init | grep -q '.' 2> /dev/null
-    if [ $? -eq 0 ]; then
-      echo "${BASEDIR} has been modified, skipping overlay update installation"
-    else
-      if [ -f "${STAGING_ROOT}/finalized/.overlay_consistent" ]; then
-        if [ ! -d /data/safe_staging/old_openpilot ]; then
-          echo "Valid overlay update found, installing"
-          LAUNCHER_LOCATION="${BASH_SOURCE[0]}"
-
-          mv $BASEDIR /data/safe_staging/old_openpilot
-          mv "${STAGING_ROOT}/finalized" $BASEDIR
-
-          # The mv changed our working directory to /data/safe_staging/old_openpilot
-          cd "${BASEDIR}"
-
-          echo "Restarting launch script ${LAUNCHER_LOCATION}"
-          exec "${LAUNCHER_LOCATION}"
-        else
-          echo "openpilot backup found, not updating"
-          # TODO: restore backup? This means the updater didn't start after swapping
-        fi
-      fi
-    fi
-  fi
+#  if [ -f "${BASEDIR}/.overlay_init" ]; then
+#    find ${BASEDIR}/.git -newer ${BASEDIR}/.overlay_init | grep -q '.' 2> /dev/null
+#    if [ $? -eq 0 ]; then
+#      echo "${BASEDIR} has been modified, skipping overlay update installation"
+#    else
+#      if [ -f "${STAGING_ROOT}/finalized/.overlay_consistent" ]; then
+#        if [ ! -d /data/safe_staging/old_openpilot ]; then
+#          echo "Valid overlay update found, installing"
+#          LAUNCHER_LOCATION="${BASH_SOURCE[0]}"
+#
+#          mv $BASEDIR /data/safe_staging/old_openpilot
+#          mv "${STAGING_ROOT}/finalized" $BASEDIR
+#
+#          # The mv changed our working directory to /data/safe_staging/old_openpilot
+#          cd "${BASEDIR}"
+#
+#          echo "Restarting launch script ${LAUNCHER_LOCATION}"
+#          exec "${LAUNCHER_LOCATION}"
+#        else
+#          echo "openpilot backup found, not updating"
+#          # TODO: restore backup? This means the updater didn't start after swapping
+#        fi
+#      fi
+#    fi
+#  fi
 
   # no cpu rationing for now
   echo 0-3 > /dev/cpuset/background/cpus
